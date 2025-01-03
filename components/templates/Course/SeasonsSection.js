fd0157ffaae024658d9573660c5197b02c37ce59
parsa <parsaes771385@gmail.com>
2025-01-03 19:36:18 +0330

    complete seasons section of course page

---
+++ components/templates/Course/SeasonsSection.js
@@ -0,0 +1,17 @@
+import React from "react";
+import SeasonRow from "./SeasonRow";
+
+function SeasonsSection() {
+  return (
+    <div className="mt-10">
+      <h5 className="font-bold text-lg">سرفصلها</h5>
+
+      <div className="mt-4 space-y-3">
+        <SeasonRow />
+        <SeasonRow />
+      </div>
+    </div>
+  );
+}
+
+export default SeasonsSection;
