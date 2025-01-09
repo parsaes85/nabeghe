1f27a94117b3fc672e7bf7308e14e0aaa50630b2
parsa <parsaes771385@gmail.com>
2025-01-09 13:18:17 +0330

    complete contact-us page

--- app/contact-us/page.js
+++ app/contact-us/page.js
@@ -1,6 +1,12 @@
 import React from "react";
+import Link from "next/link";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import TelegramIcon from "@mui/icons-material/Telegram";
+import YouTubeIcon from "@mui/icons-material/YouTube";
+
 import Navbar from "@/components/modules/Navbar";
 import Footer from "@/components/modules/Footer";
+import Form from "@/components/templates/Contact_Us/Form";
 
 function Contact_Us() {
   return (
@@ -14,7 +20,46 @@ function Contact_Us() {
           </p>
         </div>
       </div>
-      <div className="px-10 mt-10 mb-24"></div>
+      <div className="px-10 mt-10 mb-24">
+        <div className="flex items-start justify-between">
+          <div className="space-y-4">
+            <div>
+              <h3 className="font-bold text-lg mb-3 pr-2">شبکه های اجتماعی</h3>
+              <div className="flex gap-6">
+                <Link
+                  href="#"
+                  className="bg-gray-100 rounded-full p-3 text-gray-700"
+                >
+                  <InstagramIcon />
+                </Link>
+                <Link
+                  href="#"
+                  className="bg-gray-100 rounded-full p-3 text-gray-700"
+                >
+                  <TelegramIcon />
+                </Link>
+                <Link
+                  href="#"
+                  className="bg-gray-100 rounded-full p-3 text-gray-700"
+                >
+                  <YouTubeIcon />
+                </Link>
+              </div>
+            </div>
+            <div>
+              <h3 className="font-bold text-lg mb-3 pr-2">شبکه های اجتماعی</h3>
+              <p className="font-bold">۰۲۱−۱۲۳۴۵۶۷</p>
+            </div>
+            <div>
+              <h3 className="font-bold text-lg mb-3 pr-2">آدرس دفتر</h3>
+              <p className="font-bold">
+                استان، شهر، خیابان (...)، کوچه(...)، پلاک(...)
+              </p>
+            </div>
+          </div>
+          <Form />
+        </div>
+      </div>
       <Footer />
     </>
   );
---
+++ components/templates/Contact_Us/Form.js
@@ -0,0 +1,50 @@
+import React from "react";
+
+function Form() {
+  return (
+    <div className="bg-gradient-to-b from-gray-100 to-white rounded-2xl p-5 pt-0 w-[420px]">
+      <div className="bg-white rounded-b-3xl px-6 py-4">
+        <h5 className="font-bold text-[17px]">فرم تماس با ما</h5>
+      </div>
+      <form className="space-y-3 mt-4">
+        <div>
+          <label htmlFor="fullName" className="text-gray-500 text-sm">
+            نام و نام خانوادگی
+          </label>
+          <input
+            type="text"
+            id="fullName"
+            className="w-full border bg-gray-100 rounded-xl outline-none p-2 mt-2"
+          />
+        </div>
+        <div>
+          <label htmlFor="email" className="text-gray-500 text-sm">
+            ایمیل
+          </label>
+          <input
+            type="text"
+            id="email"
+            className="w-full border bg-gray-100 rounded-xl outline-none p-2 mt-2"
+          />
+        </div>
+        <div>
+          <label htmlFor="email" className="text-gray-500 text-sm">
+            ایمیل
+          </label>
+          <textarea
+            id="email"
+            rows={3}
+            className="w-full border bg-gray-100 rounded-xl outline-none p-2 mt-2"
+          />
+        </div>
+        <div>
+          <button className="text-white bg-primaryBlue px-8 py-2 rounded-full hover:opacity-80 transition-all mr-auto block">
+            ارسال پیام
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default Form;
