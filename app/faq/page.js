8417862e647b25fb2c9215bdfd9b6173227b4523
parsa <parsaes771385@gmail.com>
2025-01-12 11:48:48 +0330

    responsive faq page

--- app/faq/page.js
+++ app/faq/page.js
@@ -7,12 +7,12 @@ function Faq() {
   return (
     <>
       <Navbar />
-      <div className="mt-28 px-10">
+      <div className="mt-28 px-4 md:px-10">
         <div className="flex flex-col justify-center items-center gap-4">
           <span className="bg-yellow-500 px-[13px] py-0.5 text-2xl text-white rounded-full flex items-center justify-center">
             ?
           </span>
-          <div className="bg-gradient-to-r from-yellow-300 to-white font-bold text-2xl py-4 px-8">
+          <div className="bg-gradient-to-r from-yellow-300 to-white font-bold text-xl sm:text-2xl py-4 px-8">
             پرسش های متداول
           </div>
           <div className="flex items-center justify-center gap-2">
--- components/templates/Faq/AccordionItem.js
+++ components/templates/Faq/AccordionItem.js
@@ -8,18 +8,18 @@ function AccordionItem({ title, desc }) {
   return (
     <div className="border-b">
       <div
-        className="flex justify-between items-center cursor-pointer py-3"
+        className="flex justify-between items-center cursor-pointer py-3 gap-2"
         onClick={() => setIsAccordionOpen(!isAccordionOpen)}
       >
-        <p className="font-bold">{title}</p>
+        <p className="text-sm xs:text-base font-bold">{title}</p>
         <span>
           <ArrowForwardIosIcon
-            fontSize="small"
-            className={`${isAccordionOpen ? "-rotate-90" : "rotate-90"} transition-all `}
+            fontSize=""
+            className={`${isAccordionOpen ? "-rotate-90" : "rotate-90"} transition-all xs:text-xl`}
           />
         </span>
       </div>
-      <div className={`pb-2 text-gray-600 ${!isAccordionOpen && "hidden"}`}>
+      <div className={`text-sm xs:text-base pb-2 text-gray-600 ${!isAccordionOpen && "hidden"}`}>
         {desc}
       </div>
     </div>
